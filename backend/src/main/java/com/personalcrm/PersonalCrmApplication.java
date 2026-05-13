c44a89f823d90fe8d7ab4c8efb491752e13e080e
Gustavo Brian <gbmrp21@gmail.com>
2026-05-13 19:16:13 -0300

    Set up Spring Boot backend

---
+++ backend/src/main/java/com/personalcrm/PersonalCrmApplication.java
@@ -0,0 +1,12 @@
+package com.personalcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalCrmApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/personalcrm/PersonalCrmApplicationTests.java
@@ -0,0 +1,12 @@
+package com.personalcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
